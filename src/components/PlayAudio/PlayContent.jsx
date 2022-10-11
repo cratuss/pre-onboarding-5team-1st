8ec7fa09c0dc8d3fea3507b87d786676b4117736
cratuss <ldoh92@gmail.com>
2022-10-11 19:48:52 +0900

    [Add] react-audio-player라이브러리 추가

--- src/components/PlayAudio/PlayContent.jsx
+++ src/components/PlayAudio/PlayContent.jsx
@@ -1,7 +1,14 @@
 import styled from 'styled-components';
+import ReactAudioPlayer from 'react-audio-player';
+
+import test from '../../assets/audio/test.mp3';
 
 const PlayContent = () => {
-  return <PlayContentBlock></PlayContentBlock>;
+  return (
+    <PlayContentBlock>
+      <ReactAudioPlayer src='../../assets/audio/test.mp3' autoPlay controls />
+    </PlayContentBlock>
+  );
 };
 
 const PlayContentBlock = styled.div``;
