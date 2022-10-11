4a268c345d3f0c7f0879550ab2f300e927bc1cdb
💗 쓸💗 <sseul@ui-MacBookAir.local>
2022-10-11 23:07:43 +0900

    Add: recording 기본 기능, 사용자 승인받기 기능 구현

--- src/pages/Record.jsx
+++ src/pages/Record.jsx
@@ -1,9 +1,49 @@
 import styled from 'styled-components';
-
-const RecordBlock = styled.div``;
+import React, { useState, useCallback } from 'react';
 
 const Record = () => {
-  return <RecordBlock>Record</RecordBlock>;
+  const [stream, setStream] = useState();
+  const [source, setSource] = useState();
+  const [media, setMedia] = useState();
+  const [onRec, setOnRec] = useState(true);
+  const [analyser, setAnalyser] = useState();
+  const [audioUrl, setAudioUrl] = useState();
+
+  const onRecAudio = () => {
+    const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+    const analyser = audioCtx.createScriptProcessor(0, 1, 1);
+    setAnalyser(analyser);
+
+    function makeSound(stream) {
+      const source = audioCtx.createMediaStreamSource(stream);
+      setSource(source);
+
+      source.connect(analyser);
+      analyser.connect(audioCtx.destination);
+    }
+
+    navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
+      const mediaRecorder = newMediaRecorder(stream);
+      mediaRecorder.start();
+      setStream(stream);
+      setMedia(mediaRecorder);
+      makeSound(stream);
+      analyser.onaudioprocess = function (e) {
+        setOnRec(false);
+      };
+    });
+  };
+
+  return (
+    <>
+      {/* <RecordBlock>Record</RecordBlock>; */}
+      <button onClick={onRecAudio}>녹음</button>
+      <button>결과 확인</button>
+    </>
+  );
 };
+
+// const RecordBlock = styled.div``;
 
 export default Record;
