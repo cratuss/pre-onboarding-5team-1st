c55ff4dfd845739cc22741df36ab4a354f74ccdf
Lee dongho <88419431+cratuss@users.noreply.github.com>
2022-10-13 00:54:38 +0900

    Feature/play audio (#5)
    [Add] play페이지UI,기능 + 기존 list페이지와 연동한 것 병합

---
+++ src/components/PlayAudio/AudioPlayer.jsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+import { useRef } from 'react';
+import ReactAudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+
+const AudioPlayer = ({ setPlaying, setTime, fileUrl, fileTitle }) => {
+  const player = useRef();
+
+  return (
+    <AudioPlayerBlock>
+      <ReactAudioPlayer
+        style={{ boxShadow: 'none', padding: '15px 0 0 0' }}
+        src={fileUrl}
+        onPlay={() => {
+          setPlaying(true);
+        }}
+        onPause={() => setPlaying(false)}
+        volume={1}
+        progressUpdateInterval={20}
+        progressJumpStep={5000}
+        header={fileTitle}
+        ref={player}
+        listenInterval={1000}
+        onSeeking={() => setTime(player.current.audio.current.currentTime)}
+        // other props here
+      />
+    </AudioPlayerBlock>
+  );
+};
+
+const AudioPlayerBlock = styled.div`
+  box-shadow: 0 5px 5px -5px #ddd;
+`;
+
+export default AudioPlayer;
